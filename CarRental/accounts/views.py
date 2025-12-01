2c51b38792a62c90a9b458ebea67047339ecac97
Abdulrahman Alhrbi <AbdullrahmanTurki@outlook.com>
2025-12-01 09:06:55 +0300

    Project start

---
+++ CarRental/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login_view(requset): 
+    pass
+
+def register_view(requset): 
+    pass
+
+def logout_view(requset):
+    pass

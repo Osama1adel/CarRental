afbda0c414a41d61ae68d44a6f10ee744f53fd71
abdulmajeed <mjeed.f.b@outlook.sa>
2025-12-01 11:23:13 +0300

    create html pages and urls and views for booking app

---
+++ CarRental/bookings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "name"
+urlpatterns = [
+    path("booking_success/", views.booking_success, name="booking_success"),
+    path("create_booking/", views.create_booking, name="create_booking"),
+    path("reviewer_dashboard/", views.reviewer_dashboard, name="reviewer_dashboard"),
+
+]
--- CarRental/bookings/views.py
+++ CarRental/bookings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def booking_success(request : HttpRequest):
+    pass
+
+def create_booking(request : HttpRequest):
+    pass
+
+def reviewer_dashboard(request : HttpRequest):
+    pass
+

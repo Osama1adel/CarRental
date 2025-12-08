525281025d241235ee847e4634029ba72955e9ad
test <1osama1adel1@gmail.com>
2025-12-08 10:22:52 +0300

    modify set

--- CarRental/CarRental/settings.py
+++ CarRental/CarRental/settings.py
@@ -31,8 +31,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://*.ngrok-free.dev',
     'https://*.ngrok-free.app', # إذا كنت تستخدم الرابط القديم
 ]
-ALLOWED_HOSTS = ['*'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -137,9 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
